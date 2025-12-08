c416bbac2cca0074e22fe9489bdb76f2f494e614
Jackson Barr <jacksonbarr2021@gmail.com>
2025-12-07 23:04:28 -0500

    Neovim: catppuccino

--- .config/nvim/lua/jacksonbarr1/init.lua
+++ .config/nvim/lua/jacksonbarr1/init.lua
@@ -1,3 +1,9 @@
 require("jacksonbarr1.options")
-
 require("jacksonbarr1.lazy_init")
+
+require("catppuccin").setup({
+    flavour = "frappe",
+    transparent_background = true
+})
+
+vim.cmd.colorscheme("catppuccin-frappe")
