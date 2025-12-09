b00844426910e0f4d6039d88a892d7f9aa85a446
Jackson Barr <jacksonbarr2021@gmail.com>
2025-12-08 22:37:34 -0500

    Neotree config additions

--- .config/nvim/lua/jacksonbarr1/plugins/neo-tree.lua
+++ .config/nvim/lua/jacksonbarr1/plugins/neo-tree.lua
@@ -9,6 +9,30 @@ return {
 		},
 		lazy = false,
 		config = function()
+			require("neo-tree").setup({
+				close_if_last_window = true,
+				popup_border_style = "rounded",
+				enable_git_status = true,
+				enable_diagnostics = true,
+				default_component_configs = {
+					container = {
+						enable_character_fade = true,
+					},
+					indent = {
+						indent_size = 2,
+						padding = 1,
+					},
+					modified = {
+						symbol = "[+]",
+						highlight = "NeoTreeModified",
+					},
+					name = {
+						trailing_slash = false,
+						use_git_status_colors = true,
+						highlight = "NeoTreeFileName",
+					},
+				},
+			})
 			vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", { silent = true })
 		end,
 	},
